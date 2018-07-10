b6fe05c43aab177053ddf5f028c15e08c04f7b0a
unknown <ghaida1234567@gmail.com>
2018-07-10 06:54:22 +0300

    my views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,16 @@
 import views
 from django.conf.urls import url
 
-
-    url(r'^$', views.girls2),
-    url(r'^hi/$', views.girls3),
+urlspatterns= [
+    url (r'(?P<article_id>\d+)/add/$' , views.add , name ="add institute"),
+    url (r'(?P<article_id>\d+)/edit/$' , views.edit , name ="edit institute"),
+    url (r'(?P<article_id>\d+)/delete/$' , views.delete , name ="delete institute"),
+    url (r'(?P<article_id>\d+)/add/$' , views.add , name ="add comments"),
+    url (r'(?P<article_id>\d+)/edit/$' , views.edit , name ="edit comments"),
+    url (r'(?P<article_id>\d+)/delete/$' , views.delete , name ="delete comments"),
+    url (r'(?P<article_id>\d+)/add/$' , views.add , name ="add volunteer work"),
+    url (r'(?P<article_id>\d+)/edit/$' , views.edit , name ="edit volunteer work"),
+    url (r'(?P<article_id>\d+)/delete/$' , views.delete , name ="delete volunteer work"),
     url (r'(?P<article_id>\d+)/$' , views.show , name ="show") ,
-    url (r'^list$', views.list) ,
-     url (r'^survey$', views.survey) ,
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from models import ?
+from models import institute
+from models import comments
+from models import volunteer_work
 
 def index(request):
-    return HttpResponse ("الصفحة الرئيسية")
+     return render (request, "accounts/index.html" )
 
-def institute (request, article_id):
-    name = .objects.get
-    description = {"":}
-    return render (request, "/institute.html" ,description)
+def add_institute (request):
+    name = institute.objects.create
+    context = {"institute": institute}
+    return render (request, "accounts/institute.html" ,context)
 
-def comment(request):
+def edit_institute (request, institute_id):
+    name = institute.objects.get(pk = institute_id)
+    context = {"institute": institute}
+    return render (request, "accounts/institute.html" ,context)
+
+def delete_institute (request):
+    name = institute.objects.delete
+    context = {"institute": institute}
+    return render (request, "accounts/institute.html" ,context)
+
+def add_comment(request):
     text = request.POST['text']
     author = request.POST['author']
-    date =
 
-def volunteer_work(request) :
-    name = .objects.get
-    description = {"":}
-    return render (request, "/volunteer_work.html" ,description)
+def edit_comment(request):
+    text = request.POST['text']
+    author = request.POST['author']
 
+def delete_comment(request):
+    text = request.POST['text']
+    author = request.POST['author']
+
+def add_volunteer_work(request) :
+    name = volunteer_work.objects.create
+    context = {"volunteer_work":volunteer_work}
+    return render (request, "accounts/volunteer_work.html" ,context)
+
+def edit_volunteer_work(request, volunteer_work_id) :
+    name = volunteer_work.objects.get(pk = volunteer_work_id)
+    context = {"volunteer_work":volunteer_work}
+    return render (request, "accounts/volunteer_work.html" ,context)
+
+def delete_volunteer_work(request) :
+    name = volunteer_work.objects.delete
+    context = {"volunteer_work":volunteer_work}
+    return render (request, "accounts/volunteer_work.html" ,context)
+
+def show (request, institute_id):
+    article = show.objects.get(pk = institute_id)
+    context = {"institute": institute}
+    return render (request, "accounts/show.html" ,context)
